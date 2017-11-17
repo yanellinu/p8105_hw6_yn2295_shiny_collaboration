3f46f3a08ea063c8b3a9936fef66e937282e75d5
Yanelli nunez <yn22@cumc.columbia.edu>
2017-11-17 10:31:07 -0500

    updated file name from template to yn2295

--- p8105_hw6_yn2295_shiny/app.R
+++ p8105_hw6_yn2295_shiny/app.R
@@ -2,4 +2,5 @@ rsconnect::setAccountInfo(name='yanellinunez',
                           token='2D0770F020B5269757197C95A3087F9B',
                           secret='m3JiwFQv8DJ0VycL8fKgOMQF4DULolVJ/8PKYvRy')
 
-rsconnect::deployApp('./p8105_hw_yn2295_shiny.Rmd')
+rsconnect::deployApp('./shiny_dashboard_yn2295.Rmd')
+
